8909f82bea67682ea9f30273fb16a2338afb93d4
Mina Nilay TEZER <107746643+Mimakk@users.noreply.github.com>
2023-05-12 05:41:07 +0000

    ading scroll button

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+let scrollContainer = document.querySelector('.scroll');
+let btn = document.querySelector('.forarrow');
 
+scrollContainer.addEventListener("wheel", (event) => {
+    scrollContainer.scrollLeft += event.deltaY;
+});
+
+btn.addEventListener("click", () => {
+    scrollContainer.scrollLeft += 350;
+});
